86bb53df13c0dd1e3496d40b29b477cceb1c8613
LC1B_04_Izumida_Masato <k021g1329@g.neec.ac.jp>
2022-07-20 18:46:24 +0900

    頑張れ

--- WinApp.cpp
+++ WinApp.cpp
@@ -1,11 +1,11 @@
-#include"WinApi.h"
+#include"WinApp.h"
 
 
 // @brief コンソール画面にフォーマット付き文字列の表示
 // @param format フォーマット(%dとか%fとかの)
 // @param 可変長引数
 // @remarks この関数はデバック用です。デバッグ時にしか動作しません
-void WinApi::DebugOutputFormatString(const char* format, ...) {
+void WinApp::DebugOutputFormatString(const char* format, ...) {
 #ifdef _DEBUG
 	va_list valist;
 	va_start(valist, format);
@@ -14,7 +14,7 @@ void WinApi::DebugOutputFormatString(const char* format, ...) {
 #endif
 }
 
-LRESULT WinApi::WindowProcedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
+LRESULT WinApp::WindowProcedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
 	//ウィンドウが破壊されたら呼ばれる
 	if (msg == WM_DESTROY) {
 		PostQuitMessage(0);//OSに対して「このアプリはもう終わる」と伝える
@@ -23,58 +23,40 @@ LRESULT WinApi::WindowProcedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lpara
 	return DefWindowProc(hwnd, msg, wparam, lparam);
 }
 
-WNDCLASSEX WinApi::W()
-{
-	return this->w;
-}
 
-HWND WinApi::Hwnd()
-{
-	return this->hwnd;
-}
-
-MSG WinApi::Msg()
+MSG WinApp::Msg()
 {
 	return this->msg;
 }
 
-int WinApi::Window_width()
-{
-	return window_width;
-}
-
-int WinApi::Window_height()
-{
-	return window_height;
-}
 
 
-WinApi::WinApi() {
+WinApp::WinApp() {
 
 }
 
-void WinApi::WinApiInitialize()
+void WinApp::WinApiInitialize()
 {
 
 #pragma region WindowsAPI初期化処理
 
 
-	w = {};
-	w.cbSize = sizeof(WNDCLASSEX);
-	w.lpfnWndProc = (WNDPROC)WinApi::WindowProcedure;	//ウィンドウプロシージャを設定
-	w.lpszClassName = _T("DX12Sample");			//ウィンドウクラス名
-	w.hInstance = GetModuleHandle(nullptr);		//ウィンドウハンドル
-	w.hCursor = LoadCursor(NULL, IDC_ARROW);	//カーソル指定
+	wndClass_ = {};
+	wndClass_.cbSize = sizeof(WNDCLASSEX);
+	wndClass_.lpfnWndProc = (WNDPROC)WinApp::WindowProcedure;	//ウィンドウプロシージャを設定
+	wndClass_.lpszClassName = _T("DX12Sample");			//ウィンドウクラス名
+	wndClass_.hInstance = GetModuleHandle(nullptr);		//ウィンドウハンドル
+	wndClass_.hCursor = LoadCursor(NULL, IDC_ARROW);	//カーソル指定
 
 	//ウィンドウクラスをOSに登録する
-	RegisterClassEx(&w);
+	RegisterClassEx(&wndClass_);
 	//ウィンドウサイズ{X座標　Y座標　横幅　縦幅}
-	wrc = { 0,0,window_width,window_height };
+	wrc = { 0,0,kWindowWidth,kWindowHeight };
 	//関数を使ってウィンドウのサイズを自動で補正する
 	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
 
 	//ウィンドウオブジェクトの生成
-	hwnd = CreateWindow(w.lpszClassName,//クラス名指定
+	hwnd_ = CreateWindow(wndClass_.lpszClassName,//クラス名指定
 		_T("LE2B_16_イズミダ_マサト_AL3"),					//タイトルバーの文字
 		WS_OVERLAPPEDWINDOW,			//タイトルバーと境界線があるウィンドウ
 		CW_USEDEFAULT,					//表示x座標はOSにお任せ
@@ -83,11 +65,11 @@ void WinApi::WinApiInitialize()
 		wrc.bottom - wrc.top,			//ウィンドウ高
 		nullptr,						//親ウィンドウハンドル
 		nullptr,						//メニューハンドル
-		w.hInstance,					//呼び出しアプリケーションハンドル
+		wndClass_.hInstance,					//呼び出しアプリケーションハンドル
 		nullptr);						//追加パラメーター(オプション)
 
 	//ウィンドウ表示
-	ShowWindow(hwnd, SW_SHOW);
+	ShowWindow(hwnd_, SW_SHOW);
 
 	msg = {};
 
@@ -95,7 +77,7 @@ void WinApi::WinApiInitialize()
 
 }
 
-void WinApi::Updata() {
+void WinApp::Updata() {
 
 	if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
 		TranslateMessage(&msg);
@@ -103,3 +85,18 @@ void WinApi::Updata() {
 	}
 
 }
+
+void WinApp::WinAppFinish()
+{
+
+#pragma region  WindowsAPI後始末
+
+	//もうクラスは使わないので登録を解除する
+	UnregisterClass(wndClass_.lpszClassName, wndClass_.hInstance);
+
+#pragma endregion
+
+}
+
+
+
---
+++ WinApp.h
@@ -0,0 +1,46 @@
+#pragma once
+#include<DirectXMath.h>
+#include<Windows.h>
+#include <DirectXTex.h>
+#include <tchar.h>
+
+
+
+class WinApp {
+
+public:
+	// ウィンドウサイズ
+	static const int kWindowWidth = 1280; // 横幅
+	static const int kWindowHeight = 720; // 縦幅
+	// ウィンドウクラス名
+	static const wchar_t kWindowClassName[];
+
+	enum class SizeChangeMode {
+		kNone,        //!< サイズ変更不可
+		kNormal,      //!< 自由変更
+		kFixedAspect, //!< アスペクト比一定
+	};
+
+public://GetterSettter登録
+	HINSTANCE GetHInstance() const { return wndClass_.hInstance; };
+	HWND GetHwnd() const { return hwnd_; };
+	MSG Msg();
+
+public://関数群
+	WinApp();
+	void WinApiInitialize();
+
+	void DebugOutputFormatString(const char* format, ...);
+	static LRESULT WindowProcedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
+	void Updata();
+
+	void WinAppFinish();
+
+private:
+	
+
+	WNDCLASSEX wndClass_{};
+	RECT wrc;
+	HWND hwnd_;
+	MSG msg;
+};

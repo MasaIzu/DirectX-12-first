4b7b683325bb079ec00fa9654cb174c44224ac28
masato izumida <Oioi_0126@outlook.com>
2022-10-06 23:42:55 +0900

    マトリックス4

--- Matrix4.cpp
+++ Matrix4.cpp
@@ -1,139 +1,211 @@
-//#include"Matrix4.h"
-//#include "cmath"
-//
-//Matrix4::Matrix4() {
-//
-//}
-//
-//
-//
-////単位行列を設定
-//Matrix4 Matrix4::identity()
-//{
-//	static const Matrix4 result
-//	{
-//		1.0f,0.0f,0.0f,0.0f,
-//		0.0f,1.0f,0.0f,0.0f,
-//		0.0f,0.0f,1.0f,0.0f,
-//		0.0f,0.0f,0.0f,1.0f,
-//	};
-//	return result;
-//}
-////拡大縮小行列の設定
-//Matrix4 scale(const Vector3& s)
-//{
-//	Matrix4 result
-//	{
-//		s.x, 0.0f,0.0f,0.0f,
-//		0.0f,s.y, 0.0f,0.0f,
-//		0.0f,0.0f,s.z, 0.0f,
-//		0.0f,0.0f,0.0f,1.0f,
-//	};
-//	return result;
-//}
-////回転行列の設定
-//Matrix4 rotateX(float angle)
-//{
-//	float sin = std::sin(angle);
-//	float cos = std::cos(angle);
-//
-//	Matrix4 result
-//	{
-//		1.0f,0.0f,0.0f,0.0f,
-//		0.0f,cos, sin, 0.0f,
-//		0.0f,-sin,cos, 0.0f,
-//		0.0f,0.0f,0.0f,1.0f,
-//	};
-//	return result;
-//
-//}
-//Matrix4 rotateY(float angle)
-//{
-//	float sin = std::sin(angle);
-//	float cos = std::cos(angle);
-//
-//	Matrix4 result
-//	{
-//		cos, 0.0f,sin, 0.0f,
-//		0.0f,1.0f,0.0f,0.0f,
-//		-sin,0.0f,cos, 0.0f,
-//		0.0f,0.0f,0.0f,1.0f,
-//	};
-//	return result;
-//
-//}
-//Matrix4 rotateZ(float angle)
-//{
-//	float sin = std::sin(angle);
-//	float cos = std::cos(angle);
-//
-//	Matrix4 result
-//	{
-//		cos, sin, 0.0f,0.0f,
-//		-sin,cos, 0.0f,0.0f,
-//		0.0f,0.0f,1.0f,0.0f,
-//		0.0f,0.0f,0.0f,1.0f,
-//	};
-//	return result;
-//
-//}
-////平行移動行列の設定
-//Matrix4 translate(const Vector3& t)
-//{
-//	Matrix4 result
-//	{
-//		1.0f, 0.0f, 0.0f, 0.0f,
-//		0.0f, 1.0f, 0.0f, 0.0f,
-//		0.0f, 0.0f, 1.0f, 0.0f,
-//		t.x,  t.y,  t.z,  1.0f,
-//	};
-//	return result;
-//}
-////座標変換
-//Vector3 transform(const Vector3& v, const Matrix4& m)
-//{
-//	float w = v.x * m.m[0][3] + v.y * m.m[1][3] + v.z * m.m[2][3] + m.m[3][3];
-//
-//	Vector3 result
-//	{
-//		(v.x * m.m[0][0] + v.y * m.m[1][0] + v.z * m.m[2][0] + m.m[3][0]) / w,
-//		(v.x * m.m[0][1] + v.y * m.m[1][1] + v.z * m.m[2][1] + m.m[3][1]) / w,
-//		(v.x * m.m[0][2] + v.y * m.m[1][2] + v.z * m.m[2][2] + m.m[3][2]) / w,
-//	};
-//	return result;
-//}
-////代入演算子オーバーロード
-//Matrix4& operator *=(Matrix4& m1, const Matrix4& m2)
-//{
-//	Matrix4 result{ 0 };
-//
-//	for (int i = 0; i < 4; i++)
-//	{
-//		for (int j = 0; j < 4; j++)
-//		{
-//			for (int k = 0; k < 4; k++)
-//			{
-//				result.m[i][j]
-//					= m1.m[i][0] * m2.m[0][j]
-//					+ m1.m[i][1] * m2.m[1][j]
-//					+ m1.m[i][2] * m2.m[2][j]
-//					+ m1.m[i][3] * m2.m[3][j];
-//			}
-//
-//		}
-//
-//	}
-//	m1 = result;
-//	return result;
-//}
-//
-////2項演算子オーバーロード
-//Matrix4 operator *(const Matrix4& m1, const Matrix4& m2)
-//{
-//	Matrix4 result = m1;
-//	return result *= m2;
-//}
-//Vector3 operator *(const Vector3& v, const Matrix4& m)
-//{
-//	return transform(v, m);
-//}
+#include "Matrix4.h"
+#include <cmath>
+
+Matrix4::Matrix4()
+{
+	float result[4][4]
+	{
+		1.0f,0.0f,0.0f,0.0f,
+		0.0f,1.0f,0.0f,0.0f,
+		0.0f,0.0f,1.0f,0.0f,
+		0.0f,0.0f,0.0f,1.0f
+	};
+	// 代入
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			m[i][j] = result[i][j];
+		}
+	}
+}
+
+//単位行列を求める
+Matrix4 Matrix4::identity()
+{
+	float result[4][4]
+	{
+		1.0f,0.0f,0.0f,0.0f,
+		0.0f,1.0f,0.0f,0.0f,
+		0.0f,0.0f,1.0f,0.0f,
+		0.0f,0.0f,0.0f,1.0f
+	};
+
+	// 代入
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			m[i][j] = result[i][j];
+		}
+	}
+
+	return *this;
+}
+
+//拡大縮小行列を求める
+Matrix4 Matrix4::scale(const Vector3& s)
+{
+	float result[4][4]
+	{
+		s.x ,0.0f,0.0f,0.0f,
+		0.0f,s.y ,0.0f,0.0f,
+		0.0f,0.0f,s.z ,0.0f,
+		0.0f,0.0f,0.0f,1.0f
+	};
+
+	// 代入
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			m[i][j] = result[i][j];
+		}
+	}
+
+	return *this;
+}
+
+// x 軸周りの回転行列を求める
+Matrix4 Matrix4::rotateX(float angle)
+{
+	float sin = std::sin(angle);
+	float cos = std::cos(angle);
+
+	float result[4][4]
+	{
+		1.0f,0.0f,0.0f,0.0f,
+		0.0f, cos, sin,0.0f,
+		0.0f,-sin, cos,0.0f,
+		0.0f,0.0f,0.0f,1.0f
+	};
+
+	// 代入
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			m[i][j] = result[i][j];
+		}
+	}
+
+	return *this;
+}
+
+// y 軸周りの回転行列を求める
+Matrix4 Matrix4::rotateY(float angle)
+{
+	float sin = std::sin(angle);
+	float cos = std::cos(angle);
+
+	float result[4][4]
+	{
+		cos ,0.0f,-sin,0.0f,
+		0.0f,1.0f,0.0f,0.0f,
+		sin ,0.0f, cos,0.0f,
+		0.0f,0.0f,0.0f,1.0f
+	};
+
+	// 代入
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			m[i][j] = result[i][j];
+		}
+	}
+
+	return *this;
+}
+
+// z 軸周りの回転行列を求める
+Matrix4 Matrix4::rotateZ(float angle)
+{
+	float sin = std::sin(angle);
+	float cos = std::cos(angle);
+
+	float result[4][4]
+	{
+		cos , sin,0.0f,0.0f,
+		-sin, cos,0.0f,0.0f,
+		0.0f,0.0f,1.0f,0.0f,
+		0.0f,0.0f,0.0f,1.0f
+	};
+
+	// 代入
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			m[i][j] = result[i][j];
+		}
+	}
+
+	return *this;
+}
+
+// 平行移動行列を求める
+Matrix4 Matrix4::translate(const Vector3& t)
+{
+	float result[4][4]
+	{
+		1.0f,1.0f,0.0f,0.0f,
+		0.0f,1.0f,0.0f,0.0f,
+		0.0f,0.0f,1.0f,0.0f,
+		t.x ,t.y ,t.z ,1.0f
+	};
+
+	// 代入
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			m[i][j] = result[i][j];
+		}
+	}
+
+	return *this;
+}
+
+// 座標変換（ベクトルと行列の掛け算）を行うtransform 関数を作成する　(透視変換にも対応している)
+Vector3 Matrix4::transform(const Vector3& v, const Matrix4& m)
+{
+	float W = v.x * m.m[0][3] + v.y * m.m[1][3] + v.z * m.m[2][3] + m.m[3][3];
+	Vector3 result
+	{
+		(v.x * m.m[0][0] + v.y * m.m[1][0] + v.z * m.m[2][0] + m.m[3][0]) / W,
+		(v.x * m.m[0][1] + v.y * m.m[1][1] + v.z * m.m[2][1] + m.m[3][1]) / W,
+		(v.x * m.m[0][2] + v.y * m.m[1][2] + v.z * m.m[2][2] + m.m[3][2]) / W
+	};
+
+	return result;
+}
+
+// 代入演算子　*=　オーバーロード関数（行列と行列の積）
+Matrix4& Matrix4::operator*=(const Matrix4& m1)
+{
+	Matrix4 result;
+
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			float Total = 0.0f;
+			for (int k = 0; k < 4; k++) {
+				Total += m[i][k] * m1.m[k][j];
+			}
+			result.m[i][j] = Total;
+		}
+	}
+
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			m[i][j] = result.m[i][j];
+		}
+	}
+
+	return *this;
+}
+
+// ２項演算子　*　のオーバーロード関数（行列と行列の積）
+Matrix4 Matrix4::operator*(const Matrix4& m1)
+{
+	return *this *= m1;
+}
+
+// ２項演算子　*　のオーバーロード関数（ベクトルと行列の積）
+Vector3 operator*(const Vector3& v, const Matrix4& m)
+{
+	float W = v.x * m.m[0][3] + v.y * m.m[1][3] + v.z * m.m[2][3] + m.m[3][3];
+	Vector3 result
+	{
+		(v.x * m.m[0][0] + v.y * m.m[1][0] + v.z * m.m[2][0] + m.m[3][0]) / W,
+		(v.x * m.m[0][1] + v.y * m.m[1][1] + v.z * m.m[2][1] + m.m[3][1]) / W,
+		(v.x * m.m[0][2] + v.y * m.m[1][2] + v.z * m.m[2][2] + m.m[3][2]) / W
+	};
+	return result;
+}
--- Matrix4.h
+++ Matrix4.h
@@ -1,36 +1,40 @@
-//#pragma once
-//#include "Vector3.h"
-//
-//class Matrix4 {
-//
-//public:
-//
-//	float m[4][4];
-//
-//	Matrix4();
-//
-//	//単位行列を設定
-//	Matrix4 Identity();
-//
-//	//拡大縮小行列の設定
-//	Matrix4 Scale(const Vector3& s);
-//
-//	//回転行列の設定
-//	Matrix4 RotateX(float angle);
-//	Matrix4 RotateY(float angle);
-//	Matrix4 RotateZ(float angle);
-//
-//	//平行移動行列の設定
-//	Matrix4 Translate(const Vector3& t);
-//
-//	//座標変換
-//	Vector3 Transform(const Vector3& v, const Matrix4& m);
-//
-//	//代入演算子オーバーロード
-//	Elements& operator *=(Elements& m1);
-//
-//	//2項演算子オーバーロード
-//	Matrix4 operator *(const Matrix4& m1, const Matrix4& m2);
-//	Vector3 operator *(const Vector3& v, const Matrix4& m);
-//
-//};
+#pragma once
+#include "Vector3.h"
+
+class Matrix4
+{
+public:
+	// Matrix4　構造体
+	float m[4][4];
+
+	Matrix4();
+
+	// 単位行列を求める
+	Matrix4 identity();
+
+	//拡大縮小行列の設定
+	Matrix4 scale(const Vector3& s);
+
+	// 回転行列の設定
+	Matrix4 rotateX(float angle);
+	Matrix4 rotateY(float angle);
+	Matrix4 rotateZ(float angle);
+
+	// 平行移動行列の作成
+	Matrix4 translate(const Vector3& t);
+
+	// 座標変換（ベクトルと行列の掛け算をする）
+	Vector3 transform(const Vector3& v, const Matrix4& m);
+
+	// 代入演算子オーバーロード
+	Matrix4& operator*=(const Matrix4& m1);
+
+	Matrix4 operator*(const Matrix4& m1);
+
+private:
+
+};
+
+// 2項演算子オーバーロード
+
+Vector3 operator*(const Vector3& v, const Matrix4& m);
